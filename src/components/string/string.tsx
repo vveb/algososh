55373cbb33df7ab1cd8af34e3d2c77e482391265
Victor Tarapata <vvteach@yandex.ru>
2024-01-29 21:07:22 +0300

    first commit

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import styles from './string.module.css';
+import { Button } from "../ui/button/button";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.container}>
+        <Input extraClass={styles.input} maxLength={11} isLimitText={true}></Input>
+        <Button text='Развернуть'></Button>
+      </div>
     </SolutionLayout>
   );
 };
